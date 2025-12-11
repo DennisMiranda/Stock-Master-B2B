a2d5fc2d617af9a8e9d6a4b422522553202f41d1
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-12-11 13:45:38 -0600

    Add frontend initial files

---
+++ frontend/stockmaster-client/src/app/features/business/logistics/components/delivery-map/delivery-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-map',
+  imports: [],
+  templateUrl: './delivery-map.html',
+  styleUrl: './delivery-map.css',
+})
+export class DeliveryMap {
+
+}
