bb20f1ff5fa9a43823062e7af27743b1d2745c79
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-12-15 13:25:55 -0600

    Update catalog page to call all components

--- frontend/stockmaster-client/src/app/features/user/catalog/pages/catalog-page/catalog-page.ts
+++ frontend/stockmaster-client/src/app/features/user/catalog/pages/catalog-page/catalog-page.ts
@@ -1,22 +1,34 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardProduct } from '../../../../../shared/ui/cards/card-product/card-product';
+import { BasicPagination } from '../../../../../shared/ui/pagination/basic-pagination/basic-pagination';
+import { ProductFilterSidebar } from '../../components/product-filter-sidebar/product-filter-sidebar';
 import { ProductSearch } from '../../components/product-search/product-search';
 import { CatalogService } from '../../services/catalog.service';
 
 @Component({
   selector: 'app-catalog-page',
-  imports: [ProductSearch],
+  imports: [ProductSearch, CardProduct, ProductFilterSidebar, BasicPagination],
   templateUrl: './catalog-page.html',
   styleUrls: ['./catalog-page.css'],
 })
-export class CatalogPage {
+export class CatalogPage implements OnInit {
   catalogService = inject(CatalogService);
+  private route = inject(ActivatedRoute); // Changed to ActivatedRoute
 
-  products: unknown[] = [];
-  loading = signal(false);
-  /**
-   * Maneja el cambio en el campo de búsqueda.
-   * @param searchTerm - El término de búsqueda ingresado por el usuario.
-   */
+  term = this.catalogService.term;
+  currentPage = this.catalogService.page;
+  products = this.catalogService.products;
+  metadata = this.catalogService.metadata;
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const page = params['page'] ? +params['page'] : 1;
+      const searchTerm = params['search'] || '';
+      this.catalogService.updateSearch({ search: searchTerm, page });
+    });
+  }
+
   onSearchChange(searchTerm: string) {
     this.catalogService.searchProducts(searchTerm);
   }
