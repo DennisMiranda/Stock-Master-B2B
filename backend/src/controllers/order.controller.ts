e0bbb641461dea046531b41bef3be0355e144761
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-12-23 20:52:16 -0600

    Add get orders and order by user id

--- backend/src/controllers/order.controller.ts
+++ backend/src/controllers/order.controller.ts
@@ -33,7 +33,78 @@ class OrderController {
       console.log(error);
       res
         .status(400)
-        .json(CustomResponse.error("ORDER_ERROR", "", JSON.stringify(error)));
+        .json(CustomResponse.error("ORDER_ERROR", "Error creating order"));
+    }
+  }
+
+  async getOrders(req: Request, res: Response) {
+    try {
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+      const order = await orderService.getOrdersPaginated({
+        page,
+        limit,
+      });
+      res
+        .status(200)
+        .json(CustomResponse.success(order, "Orders fetched successfully"));
+    } catch (error) {
+      console.log(error);
+      res
+        .status(400)
+        .json(CustomResponse.error("ORDER_ERROR", "Error getting orders"));
+    }
+  }
+
+  async getOrderById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ message: "Order id is required" });
+      }
+      const order = await orderService.getOrderById(id);
+
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+
+      return res.json(
+        CustomResponse.success(order, "Order detail fetched successfully")
+      );
+    } catch (error) {
+      console.error("Error getting order by id", error);
+      return res
+        .status(500)
+        .json(CustomResponse.error("ORDER_ERROR", "Error getting order by id"));
+    }
+  }
+
+  async getByUserId(req: Request, res: Response) {
+    try {
+      const { userId } = req.params;
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+
+      if (!userId) {
+        return res.status(400).json({ message: "User id is required" });
+      }
+
+      const result = await orderService.getOrdersByUserId(userId, {
+        page,
+        limit,
+      });
+
+      return res.json(
+        CustomResponse.success(result, "Orders fetched successfully")
+      );
+    } catch (error) {
+      console.error("Error getting orders by user", error);
+      return res
+        .status(500)
+        .json(
+          CustomResponse.error("ORDER_ERROR", "Error getting orders by user")
+        );
     }
   }
 }
--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -4,5 +4,10 @@ import orderController from "../controllers/order.controller";
 const router = Router();
 
 router.post("/", (req, res) => orderController.createOrder(req, res));
+router.get("/", (req, res) => orderController.getOrders(req, res));
+router.get("/:id", (req, res) => orderController.getOrderById(req, res));
+router.get("/user/:userId", (req, res) =>
+  orderController.getByUserId(req, res)
+);
 
 export default router;
